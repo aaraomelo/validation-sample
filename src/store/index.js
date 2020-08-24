1c43c5cbe8dd87295358a57d519a94fd417a95c7
Aarão Melo <aarao.melo.lopes@gmail.com>
2020-08-24 14:49:54 -0400

    exemplo de validação

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,66 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import { validationMixin } from 'vuelidate'
+import { alpha, required, minLength, maxLength, email } from 'vuelidate/lib/validators'
+import { getField, updateField } from 'vuex-map-fields';
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+
   state: {
+    user: {
+      name: '',
+      email: '',
+      password: ''
+    }
   },
   mutations: {
+    updateField
   },
-  actions: {
-  },
-  modules: {
+
+  getters: {
+    getField,
+    validator: (state) => new Vue({
+      mixins: [validationMixin],
+      computed: {
+        user: () => state.user
+      },
+      validations: {
+        user: {
+          name: { required, alpha, maxLength: maxLength(30), minLength: minLength(3) },
+          email: { required, email },
+          password: { required, minLength: minLength(6) }
+        }
+      }
+    }),
+
+    errors: (state, getters) => {
+
+      var user = {
+        name: [],
+        email: [],
+        password: []
+      }
+
+      getters.validator.$v.user.name.$touch();
+      !getters.validator.$v.user.name.maxLength && user.name.push("O nome deve ter no máximo 32 letras");
+      !getters.validator.$v.user.name.minLength && user.name.push("O nome deve ter no mínimo 3 letras");
+      !getters.validator.$v.user.name.alpha && user.name.push("Utilize somente letras no nome");
+      !getters.validator.$v.user.name.required && user.name.push("É necessário o nome");
+
+      getters.validator.$v.user.email.$touch();
+      !getters.validator.$v.user.email.email && user.email.push("Entre com um email válido");
+      !getters.validator.$v.user.email.required && user.email.push("É necessário o email");
+
+      getters.validator.$v.user.password.$touch();
+      !getters.validator.$v.user.password.minLength && user.password.push("A senha deve ter no mínimo 6 caracteres");
+      !getters.validator.$v.user.password.required && user.password.push("É necessário a senha");
+
+      return { user }
+    }
+
   }
 })
+
---
+++ src/views/Login.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <v-app id="inspire" dark>
+      <v-card >
+      <form class="ma-2">
+
+        <v-text-field
+          label="Nome"
+          v-model="name"
+          :error-messages="errors.user.name"
+        ></v-text-field>
+
+        <v-text-field
+          label="Email"
+          v-model="email"
+          :error-messages="errors.user.email"
+        ></v-text-field>
+
+        <v-text-field
+          label="Senha"
+          v-model="password"
+          :error-messages="errors.user.password"
+        ></v-text-field>
+         <v-btn> submit </v-btn>
+      </form>
+
+      </v-card>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import { mapFields } from 'vuex-map-fields';
+import { mapGetters } from 'vuex'
+export default {
+  computed:{
+    ...mapFields([
+      'user.name',
+      'user.email',
+      'user.password'
+    ]),
+    ...mapGetters(['errors'])
+  }
+}
+
+</script>
